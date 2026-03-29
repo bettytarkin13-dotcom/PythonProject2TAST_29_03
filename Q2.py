d144362aec28427ebe23ccaa14a126ff557a29e4
bettytarkin13-dotcom <bettytarkin13@gmail.com>
2026-03-29 19:07:03 +0300

    #2

---
+++ Q2.py
@@ -0,0 +1,14 @@
+def find_median(number:list)->float:
+    if not number:
+        raise ValueError("The list is empty")
+
+    sorted_numbers = sorted(number)
+    n=len(sorted_numbers)
+    middle=n//2
+
+    if n%2==1:
+        return float(sorted_numbers[middle])
+    else:
+        return (sorted_numbers[middle-1]+sorted_numbers[middle])/2.0
+
+print(find_median([5, 6, 5, 1]))
